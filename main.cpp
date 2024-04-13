8d959edefd0d95a7ac6adae0c6f4623c3cefa962
kiniikun <kaushal.panduru@gmail.com>
2024-04-13 10:38:00 +0000

    using recursive function

--- main.cpp
+++ main.cpp
@@ -1,24 +1,22 @@
 #include "mbed.h"
 #include <cstdio>
 
-// this a comment
-
-int t1 = 0;
-int t2 = 1;
-int next_term = t1 + t2;
 int max_terms = 8;
 
-int main()
-{
-    //this is the main function
-    printf("Hello, lab program 1 \r\n");
-    printf("Fibonacchi sequence: %d, %d, ", t1, t2);
+int fib(int n){
+    // n is the nth term result
+    if(n == 0 || n == 1)
+        return n;
+    else
+        return (fib(n-1) + fib(n-2));
+}
 
-    for(int i=3; i<=max_terms; i++){
-        printf("%d, ", next_term);
-        t1 = t2;
-        t2 = next_term;
-        next_term = t1 + t2;
+int main(){
+    //this is the main function
+    printf("Hello, lab program 1 using recursive functions \r\n");
+
+    for(int i=0; i<max_terms; i++){
+        printf("%d, ", fib(i));
     }
     printf("\r\n");
     while (true) {
